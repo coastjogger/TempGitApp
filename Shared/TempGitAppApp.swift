ba312bc2325fe6e1ee509852605bba4a3d14f1c5
coastjogger <coastjogger@gmail.com>
2020-07-22 12:56:23 +0800

    Initial Commit

---
+++ Shared/TempGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempGitAppApp.swift
+//  Shared
+//
+//  Created by cj on 22/7/20.
+//
+
+import SwiftUI
+
+@main
+struct TempGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
